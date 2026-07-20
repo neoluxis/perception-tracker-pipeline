2234c6e28621efddd2b9c7e8849e0a736b18728a
Neolux Lee <neolux_lee@outlook.com>
2026-07-20 11:18:49 +0800

    bump version to 2.0.0

---
+++ lib/cmake/kflee/kfleeTargets-noconfig.cmake
@@ -0,0 +1,269 @@
+#----------------------------------------------------------------
+# Generated CMake target import file.
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "kflee::kflee_preproc" for configuration ""
+set_property(TARGET kflee::kflee_preproc APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(kflee::kflee_preproc PROPERTIES
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libkflee_preproc.so.2.0.0"
+  IMPORTED_SONAME_NOCONFIG "libkflee_preproc.so.2"
+  )
+
+list(APPEND _cmake_import_check_targets kflee::kflee_preproc )
+list(APPEND _cmake_import_check_files_for_kflee::kflee_preproc "${_IMPORT_PREFIX}/lib/libkflee_preproc.so.2.0.0" )
+
+# Import target "kflee::kflee_prep_static" for configuration ""
+set_property(TARGET kflee::kflee_prep_static APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(kflee::kflee_prep_static PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_NOCONFIG "CXX"
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libkflee_prep_static.a"
+  )
+
+list(APPEND _cmake_import_check_targets kflee::kflee_prep_static )
+list(APPEND _cmake_import_check_files_for_kflee::kflee_prep_static "${_IMPORT_PREFIX}/lib/libkflee_prep_static.a" )
+
+# Import target "kflee::kflee_cropper" for configuration ""
+set_property(TARGET kflee::kflee_cropper APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(kflee::kflee_cropper PROPERTIES
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libkflee_cropper.so.2.0.0"
+  IMPORTED_SONAME_NOCONFIG "libkflee_cropper.so.2"
+  )
+
+list(APPEND _cmake_import_check_targets kflee::kflee_cropper )
+list(APPEND _cmake_import_check_files_for_kflee::kflee_cropper "${_IMPORT_PREFIX}/lib/libkflee_cropper.so.2.0.0" )
+
+# Import target "kflee::kflee_cropper_static" for configuration ""
+set_property(TARGET kflee::kflee_cropper_static APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(kflee::kflee_cropper_static PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_NOCONFIG "CXX"
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libkflee_cropper_static.a"
+  )
+
+list(APPEND _cmake_import_check_targets kflee::kflee_cropper_static )
+list(APPEND _cmake_import_check_files_for_kflee::kflee_cropper_static "${_IMPORT_PREFIX}/lib/libkflee_cropper_static.a" )
+
+# Import target "kflee::kflee_perception" for configuration ""
+set_property(TARGET kflee::kflee_perception APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(kflee::kflee_perception PROPERTIES
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libkflee_perception.so.2.0.0"
+  IMPORTED_SONAME_NOCONFIG "libkflee_perception.so.2"
+  )
+
+list(APPEND _cmake_import_check_targets kflee::kflee_perception )
+list(APPEND _cmake_import_check_files_for_kflee::kflee_perception "${_IMPORT_PREFIX}/lib/libkflee_perception.so.2.0.0" )
+
+# Import target "kflee::kflee_perception_static" for configuration ""
+set_property(TARGET kflee::kflee_perception_static APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(kflee::kflee_perception_static PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_NOCONFIG "CXX"
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libkflee_perception_static.a"
+  )
+
+list(APPEND _cmake_import_check_targets kflee::kflee_perception_static )
+list(APPEND _cmake_import_check_files_for_kflee::kflee_perception_static "${_IMPORT_PREFIX}/lib/libkflee_perception_static.a" )
+
+# Import target "kflee::kflee_filter" for configuration ""
+set_property(TARGET kflee::kflee_filter APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(kflee::kflee_filter PROPERTIES
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libkflee_filter.so.2.0.0"
+  IMPORTED_SONAME_NOCONFIG "libkflee_filter.so.2"
+  )
+
+list(APPEND _cmake_import_check_targets kflee::kflee_filter )
+list(APPEND _cmake_import_check_files_for_kflee::kflee_filter "${_IMPORT_PREFIX}/lib/libkflee_filter.so.2.0.0" )
+
+# Import target "kflee::kflee_filter_static" for configuration ""
+set_property(TARGET kflee::kflee_filter_static APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(kflee::kflee_filter_static PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_NOCONFIG "CXX"
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libkflee_filter_static.a"
+  )
+
+list(APPEND _cmake_import_check_targets kflee::kflee_filter_static )
+list(APPEND _cmake_import_check_files_for_kflee::kflee_filter_static "${_IMPORT_PREFIX}/lib/libkflee_filter_static.a" )
+
+# Import target "kflee::kflee_filter_kf" for configuration ""
+set_property(TARGET kflee::kflee_filter_kf APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(kflee::kflee_filter_kf PROPERTIES
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libkflee_filter_kf.so.2.0.0"
+  IMPORTED_SONAME_NOCONFIG "libkflee_filter_kf.so.2"
+  )
+
+list(APPEND _cmake_import_check_targets kflee::kflee_filter_kf )
+list(APPEND _cmake_import_check_files_for_kflee::kflee_filter_kf "${_IMPORT_PREFIX}/lib/libkflee_filter_kf.so.2.0.0" )
+
+# Import target "kflee::kflee_filter_kf_static" for configuration ""
+set_property(TARGET kflee::kflee_filter_kf_static APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(kflee::kflee_filter_kf_static PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_NOCONFIG "CXX"
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libkflee_filter_kf_static.a"
+  )
+
+list(APPEND _cmake_import_check_targets kflee::kflee_filter_kf_static )
+list(APPEND _cmake_import_check_files_for_kflee::kflee_filter_kf_static "${_IMPORT_PREFIX}/lib/libkflee_filter_kf_static.a" )
+
+# Import target "kflee::kflee_filter_ekf" for configuration ""
+set_property(TARGET kflee::kflee_filter_ekf APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(kflee::kflee_filter_ekf PROPERTIES
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libkflee_filter_ekf.so.2.0.0"
+  IMPORTED_SONAME_NOCONFIG "libkflee_filter_ekf.so.2"
+  )
+
+list(APPEND _cmake_import_check_targets kflee::kflee_filter_ekf )
+list(APPEND _cmake_import_check_files_for_kflee::kflee_filter_ekf "${_IMPORT_PREFIX}/lib/libkflee_filter_ekf.so.2.0.0" )
+
+# Import target "kflee::kflee_filter_ekf_static" for configuration ""
+set_property(TARGET kflee::kflee_filter_ekf_static APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(kflee::kflee_filter_ekf_static PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_NOCONFIG "CXX"
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libkflee_filter_ekf_static.a"
+  )
+
+list(APPEND _cmake_import_check_targets kflee::kflee_filter_ekf_static )
+list(APPEND _cmake_import_check_files_for_kflee::kflee_filter_ekf_static "${_IMPORT_PREFIX}/lib/libkflee_filter_ekf_static.a" )
+
+# Import target "kflee::kflee_filter_ukf" for configuration ""
+set_property(TARGET kflee::kflee_filter_ukf APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(kflee::kflee_filter_ukf PROPERTIES
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libkflee_filter_ukf.so.2.0.0"
+  IMPORTED_SONAME_NOCONFIG "libkflee_filter_ukf.so.2"
+  )
+
+list(APPEND _cmake_import_check_targets kflee::kflee_filter_ukf )
+list(APPEND _cmake_import_check_files_for_kflee::kflee_filter_ukf "${_IMPORT_PREFIX}/lib/libkflee_filter_ukf.so.2.0.0" )
+
+# Import target "kflee::kflee_filter_ukf_static" for configuration ""
+set_property(TARGET kflee::kflee_filter_ukf_static APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(kflee::kflee_filter_ukf_static PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_NOCONFIG "CXX"
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libkflee_filter_ukf_static.a"
+  )
+
+list(APPEND _cmake_import_check_targets kflee::kflee_filter_ukf_static )
+list(APPEND _cmake_import_check_files_for_kflee::kflee_filter_ukf_static "${_IMPORT_PREFIX}/lib/libkflee_filter_ukf_static.a" )
+
+# Import target "kflee::kflee_hungarian" for configuration ""
+set_property(TARGET kflee::kflee_hungarian APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(kflee::kflee_hungarian PROPERTIES
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libkflee_hungarian.so.2.0.0"
+  IMPORTED_SONAME_NOCONFIG "libkflee_hungarian.so.2"
+  )
+
+list(APPEND _cmake_import_check_targets kflee::kflee_hungarian )
+list(APPEND _cmake_import_check_files_for_kflee::kflee_hungarian "${_IMPORT_PREFIX}/lib/libkflee_hungarian.so.2.0.0" )
+
+# Import target "kflee::kflee_hungarian_static" for configuration ""
+set_property(TARGET kflee::kflee_hungarian_static APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(kflee::kflee_hungarian_static PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_NOCONFIG "CXX"
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libkflee_hungarian_static.a"
+  )
+
+list(APPEND _cmake_import_check_targets kflee::kflee_hungarian_static )
+list(APPEND _cmake_import_check_files_for_kflee::kflee_hungarian_static "${_IMPORT_PREFIX}/lib/libkflee_hungarian_static.a" )
+
+# Import target "kflee::kflee_mot" for configuration ""
+set_property(TARGET kflee::kflee_mot APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(kflee::kflee_mot PROPERTIES
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libkflee_mot.so.2.0.0"
+  IMPORTED_SONAME_NOCONFIG "libkflee_mot.so.2"
+  )
+
+list(APPEND _cmake_import_check_targets kflee::kflee_mot )
+list(APPEND _cmake_import_check_files_for_kflee::kflee_mot "${_IMPORT_PREFIX}/lib/libkflee_mot.so.2.0.0" )
+
+# Import target "kflee::kflee_mot_static" for configuration ""
+set_property(TARGET kflee::kflee_mot_static APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(kflee::kflee_mot_static PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_NOCONFIG "CXX"
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libkflee_mot_static.a"
+  )
+
+list(APPEND _cmake_import_check_targets kflee::kflee_mot_static )
+list(APPEND _cmake_import_check_files_for_kflee::kflee_mot_static "${_IMPORT_PREFIX}/lib/libkflee_mot_static.a" )
+
+# Import target "kflee::kflee_mot_bytetrack" for configuration ""
+set_property(TARGET kflee::kflee_mot_bytetrack APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(kflee::kflee_mot_bytetrack PROPERTIES
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libkflee_mot_bytetrack.so.2.0.0"
+  IMPORTED_SONAME_NOCONFIG "libkflee_mot_bytetrack.so.2"
+  )
+
+list(APPEND _cmake_import_check_targets kflee::kflee_mot_bytetrack )
+list(APPEND _cmake_import_check_files_for_kflee::kflee_mot_bytetrack "${_IMPORT_PREFIX}/lib/libkflee_mot_bytetrack.so.2.0.0" )
+
+# Import target "kflee::kflee_mot_bytetrack_static" for configuration ""
+set_property(TARGET kflee::kflee_mot_bytetrack_static APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(kflee::kflee_mot_bytetrack_static PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_NOCONFIG "CXX"
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libkflee_mot_bytetrack_static.a"
+  )
+
+list(APPEND _cmake_import_check_targets kflee::kflee_mot_bytetrack_static )
+list(APPEND _cmake_import_check_files_for_kflee::kflee_mot_bytetrack_static "${_IMPORT_PREFIX}/lib/libkflee_mot_bytetrack_static.a" )
+
+# Import target "kflee::kflee_dnn_onnx" for configuration ""
+set_property(TARGET kflee::kflee_dnn_onnx APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(kflee::kflee_dnn_onnx PROPERTIES
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libkflee_dnn_onnx.so.2.0.0"
+  IMPORTED_SONAME_NOCONFIG "libkflee_dnn_onnx.so.2"
+  )
+
+list(APPEND _cmake_import_check_targets kflee::kflee_dnn_onnx )
+list(APPEND _cmake_import_check_files_for_kflee::kflee_dnn_onnx "${_IMPORT_PREFIX}/lib/libkflee_dnn_onnx.so.2.0.0" )
+
+# Import target "kflee::kflee_dnn_onnx_static" for configuration ""
+set_property(TARGET kflee::kflee_dnn_onnx_static APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(kflee::kflee_dnn_onnx_static PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_NOCONFIG "CXX"
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libkflee_dnn_onnx_static.a"
+  )
+
+list(APPEND _cmake_import_check_targets kflee::kflee_dnn_onnx_static )
+list(APPEND _cmake_import_check_files_for_kflee::kflee_dnn_onnx_static "${_IMPORT_PREFIX}/lib/libkflee_dnn_onnx_static.a" )
+
+# Import target "kflee::kflee_visualization" for configuration ""
+set_property(TARGET kflee::kflee_visualization APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(kflee::kflee_visualization PROPERTIES
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libkflee_visualization.so.2.0.0"
+  IMPORTED_SONAME_NOCONFIG "libkflee_visualization.so.2"
+  )
+
+list(APPEND _cmake_import_check_targets kflee::kflee_visualization )
+list(APPEND _cmake_import_check_files_for_kflee::kflee_visualization "${_IMPORT_PREFIX}/lib/libkflee_visualization.so.2.0.0" )
+
+# Import target "kflee::kflee_visualization_static" for configuration ""
+set_property(TARGET kflee::kflee_visualization_static APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(kflee::kflee_visualization_static PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_NOCONFIG "CXX"
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libkflee_visualization_static.a"
+  )
+
+list(APPEND _cmake_import_check_targets kflee::kflee_visualization_static )
+list(APPEND _cmake_import_check_files_for_kflee::kflee_visualization_static "${_IMPORT_PREFIX}/lib/libkflee_visualization_static.a" )
+
+# Import target "kflee::kflee_pipeline" for configuration ""
+set_property(TARGET kflee::kflee_pipeline APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(kflee::kflee_pipeline PROPERTIES
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libkflee_pipeline.so.2.0.0"
+  IMPORTED_SONAME_NOCONFIG "libkflee_pipeline.so.2"
+  )
+
+list(APPEND _cmake_import_check_targets kflee::kflee_pipeline )
+list(APPEND _cmake_import_check_files_for_kflee::kflee_pipeline "${_IMPORT_PREFIX}/lib/libkflee_pipeline.so.2.0.0" )
+
+# Import target "kflee::kflee_pipeline_static" for configuration ""
+set_property(TARGET kflee::kflee_pipeline_static APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(kflee::kflee_pipeline_static PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_NOCONFIG "CXX"
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libkflee_pipeline_static.a"
+  )
+
+list(APPEND _cmake_import_check_targets kflee::kflee_pipeline_static )
+list(APPEND _cmake_import_check_files_for_kflee::kflee_pipeline_static "${_IMPORT_PREFIX}/lib/libkflee_pipeline_static.a" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
